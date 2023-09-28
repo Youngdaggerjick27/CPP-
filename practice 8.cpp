b977f63d2cdf72368bab2d41de118abd65fde535
Youngdaggerjick27 <146303000+Youngdaggerjick27@users.noreply.github.com>
2023-09-28 10:45:49 +0800

    Add files via upload

---
+++ practice 8.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	string pangalan;
+	
+	cout<< "enter your name: ";
+	getline(cin,pangalan); // para mag bu-oe it input
+	cout<<"the length of your name is: " << pangalan.size();
+   
+	
+	
+return 0;	
+}
